9ed1438edafdebd57eb4f4a47316080fcb0c5875
CaptainPhantasy <DouglasTalley1977@gmail.com>
2026-03-15 02:16:43 -0400

    fix(db): resolve duplicate column error during sqlite migrations for cache_read_tokens

--- internal/db/migrations/20260314200000_add_cache_read_tokens.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
--- +goose StatementBegin
-ALTER TABLE sessions ADD COLUMN cache_read_tokens INTEGER NOT NULL DEFAULT 0;
--- +goose StatementEnd
-
--- +goose Down
--- +goose StatementBegin
-ALTER TABLE sessions DROP COLUMN cache_read_tokens;
--- +goose StatementEnd
